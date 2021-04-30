00ec46c1049a9dbbc84fb0c24ef63bcc1e19a4e3
Ivan <tornadod@mail.ru>
2021-05-01 02:54:45 +0300

    lesson 3 quest 4 ready

---
+++ lesson3.4/Program.cs
@@ -0,0 +1,41 @@
+﻿/*«Морской бой» — вывести на экран массив 10х10, состоящий из символов X и O, 
+ * где Х — элементы кораблей, а О — свободные клетки */
+using System;
+
+namespace lesson3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] SeaWar =
+            {
+                {'O','X','X','O','O','O','O','O','O','X'},
+                {'O','O','O','O','X','X','X','O','O','X'},
+                {'X','O','O','O','O','O','O','O','O','O'},
+                {'X','O','O','O','O','O','X','O','O','O'},
+                {'X','O','X','O','O','O','O','O','O','O'},
+                {'X','O','O','O','O','O','O','O','O','O'},
+                {'O','O','O','O','O','O','O','O','X','O'},
+                {'O','O','O','O','X','X','X','O','O','O'},
+                {'O','X','X','O','O','O','O','O','O','O'},
+                {'O','O','O','O','O','O','X','O','O','O'}
+            };
+            for (int i = 0; i < SeaWar.GetLength(0); i++)
+            {
+                int j = 0;
+                while (j < SeaWar.GetLength(1))
+                {
+                    Console.Write(SeaWar[i, j]);
+                    if (SeaWar.GetLength(1) == (j+1))
+                    {
+                        Console.Write("\n");
+                    }
+                    j++;
+                }
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
